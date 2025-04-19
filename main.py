4ea60254e5b1961e3e757557225fb812dd7b2bd5
marco julio aparicio <mjaf83@gmail.com>
2025-04-19 18:59:22 +0100

    Added Rag

--- main.py
+++ main.py
@@ -12,3 +12,23 @@ load_dotenv()
 
 if __name__ == "__main__":
     print("Ingesting data...")
+    embeddings = OpenAIEmbeddings()
+    llm = ChatOpenAI(temperature=0, model="gpt-4o")
+    query = "What is Pinecone in machine learning?"
+    chain = PromptTemplate.from_template(template=query) | llm
+ 
+
+    vectorstore = PineconeVectorStore(
+        index_name=os.environ["INDEX_NAME"],
+        embedding=embeddings
+    )
+
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    combine_docs_chain = create_stuff_documents_chain(llm=llm, prompt=retrieval_qa_chat_prompt)
+    retrival_chain = create_retrieval_chain(
+        retriever=vectorstore.as_retriever(),
+        combine_docs_chain=combine_docs_chain
+    )
+
+    result = retrival_chain.invoke({"input": query})
+    print(result)
