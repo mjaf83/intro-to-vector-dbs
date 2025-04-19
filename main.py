8fc12c7b3b62d2ca8436854fa1489ac3ebcb12bc
marco julio aparicio <mjaf83@gmail.com>
2025-04-19 20:36:30 +0100

    Added manually

--- main.py
+++ main.py
@@ -4,18 +4,22 @@ from langchain_pinecone import PineconeVectorStore
 from langchain import hub
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
+from langchain_core.runnables import RunnablePassthrough
 
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
+def format_docs(docs):
+    return "\n\n".join([doc.page_content for doc in docs])
+
 if __name__ == "__main__":
     print("Ingesting data...")
     embeddings = OpenAIEmbeddings()
     llm = ChatOpenAI(temperature=0, model="gpt-4o")
     query = "What is Pinecone in machine learning?"
-    chain = PromptTemplate.from_template(template=query) | llm
+    #chain = PromptTemplate.from_template(template=query) | llm
  
 
     vectorstore = PineconeVectorStore(
@@ -32,3 +36,25 @@ if __name__ == "__main__":
 
     result = retrival_chain.invoke({"input": query})
     print(result)
+
+    template = """Use the following piece of context to answer the question at the end.
+    If you don't know the answer, just say "I don't know", don't try to make up an answer.
+    Use three sentences maximum and keep the answer as concise as possible.
+    Allways "say thanks for asking!" at the end of the answer.
+    
+    {context}
+    
+    Question: {question}
+    
+    Heppful Answer:"""
+
+    custom_rag_prompt = PromptTemplate.from_template(template=template)
+
+    rag_chain = (
+        {"context": vectorstore.as_retriever() | format_docs, "question": RunnablePassthrough()}
+        | custom_rag_prompt
+        | llm
+    )
+
+    res = rag_chain.invoke(query)
+    print(res)
